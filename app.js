a349798d2d2cb272542255632c7d336a4de5e8a2
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-04-17 05:39:46 +0400

    js task

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1.verilen saniye deyerinin nece deqiqe ve saniye oldugun hesablayan proqram yaz
+// meselen:1200 ---20deq 0san
+
+// let san=1567
+// a=san/60
+// deq=parseInt(a)
+// c=san-deq*60
+// console.log(`${deq} deq ${c} san`)
+
+
+// 2. 
+let f=100;
+let c=5/9*(f-32)
+console.log(c)
