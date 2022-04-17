112dae2fd627f9305b9d70b1081f71d3f879940e
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-04-17 13:35:25 +0400

    verilen saniyeni deqiqeye ceviren alqoiritmi daha optimal yazdim

--- app.js
+++ app.js
@@ -7,8 +7,16 @@
 // c=san-deq*60
 // console.log(`${deq} deq ${c} san`)
 
+// optimal
+let sanString=prompt("saniye daxil edin")
+let san=parseInt(sanString)
+let deq=parseInt(san/60)
+let qaliq=san%60
+alert(`${deq} deq,${qaliq} san`)
+
 
 // 2. 
-let f=100;
-let c=5/9*(f-32)
-console.log(c)
+// let f=100;
+// let c=5/9*(f-32)
+// console.log(c)
+     
