6b498467e4a64432810b7e1ab15183170e75cf15
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-05-06 10:28:18 +0400

    array uzerinde hesablama emeliyyatlari

--- app.js
+++ app.js
@@ -14,13 +14,10 @@
 // let qaliq=san%60
 // alert(`${deq} deq,${qaliq} san`)
 
-
-// 2. 
+// 2.
 // let f=100;
 // let c=5/9*(f-32)
 // console.log(c)
-
-
 
 // 3. kicik bir loterereya oyunu yaz.istifadeciden ikireqemli tesadufu ededi tapmasi teleb olunur
 // sertler:
@@ -33,7 +30,7 @@
 //     let user=prompt("size tesadufu verilmis ikireqemli ededi tapin:")
 //     let numString=num.toString()
 //     let userReverse=user[1]+user[0]
-//     if(num<10){ 
+//     if(num<10){
 //     num+=10
 //     }
 //     if(num==user){
@@ -50,14 +47,9 @@
 //     }
 // }
 
-
-
-
 // 4.butun donguler ile adinizi ekrana 5 defe yazdirin
 
-
 // let ad="Ceyhun"
-
 
 // i.
 // for(let i=0; i < 5; i++){
@@ -71,7 +63,6 @@
 //     i++
 // }
 
-
 // iii.
 // let i=0
 // do{
@@ -79,15 +70,13 @@
 //     i++
 // }while(i<5);
 
-
 // 4.  1 den 100 e  qeder olan butun ededlerin cemini tapan proqram yaz
 // let sum = 0
 // for(let i=0; i<5; i++){
 //     console.log(`${sum} + ${i} = ${sum+i}`)
 //     sum+=i
-    
-// }
 
+// }
 
 // 5. 1 den 10-a qeder ededleri for dongusu ile bir sirada aralarinda "," olacag sekilde yazin
 // let num=""
@@ -100,28 +89,20 @@
 // }
 // console.log(num)
 
-
-
-
 // 6.daxil edilen ededin faktorialini hesablayan proqram yazin
 
 // let num=parseInt(prompt("eded daxil edin"))
 // let fackt=1
 // for(let i=1; i <= num; i++){
 //     fackt = fackt * i
-    
 
 // }
 // alert(fackt)
-
-
-
 
 // 7.asagdaki f(x, y) funksiyasini daxil edilen x ve y ededleri ucun hesablayan proqram yazin
 // x>0, y<0 olarsa f(x,y)=4x+2y+4
 // x>0, y=0 olarsa f(x,y)=2x-y+3
 // x<0, y>0 olarsa f(x,y)=3x+4y+3
-
 
 // let x = parseInt(prompt("x"))
 // let y= parseInt(prompt("y"))
@@ -136,9 +117,6 @@
 //     alert(`f3(${x},${y})=3x+4y+3=${f3}`)
 // }
 
-
-
-
 // 8.vurma cedvelini gosteren bir proqram yaz
 
 // let vurmaCedveli=""
@@ -149,10 +127,6 @@
 //     console.log(vurmaCedveli)
 //     vurmaCedveli=""
 // }
-
-
-
-
 
 // 9. sistem 100-e  qeder tesadufu bir eded verir.istifadeci bu ededi tapmaga calisir ve proqram istifadeciye daxil etdiyi cavbi
 // azalt ve ya artir  deyerek komek edir.cavabi tapdigda ise nece defe cehd etdikden sonra tapdigini gosterir
@@ -177,8 +151,6 @@
 // }
 // }
 
-
-
 // 10. istifadeci muxtelif ededler daxil edir ve 0 daxil etdikde daxil edilen ededlerin hasilini gosterir(0  hasili hesabalnan ededlere daxil deyil).
 
 // let hasil=1
@@ -192,7 +164,6 @@
 //     }
 // }
 
-
 // 11. istifadecinin daxil etdiyi edede qeder butun sade ededleri cap eden proqram yaz.mes(10: 2 3 5 7 )
 
 // let user = parseInt(prompt("eded daxil edin:"))
@@ -202,25 +173,21 @@
 //     let sade=true
 
 //     for(let j=2; j<i; j++){
-         
+
 //         if(i%j == 0){
 //             sade=false
 //             break
 //         }
-        
+
 //     }
 //     if(sade){
 //         sadeEdedler+=i + ", "
 //     }
-    
 
 // }
 // console.log(sadeEdedler);
 
-
-
 // 12.verilen listdeki objectleri id e gore tapin
-
 
 // const men=[
 //     {id:1, ad:"ceyhun1"},
@@ -234,7 +201,6 @@
 //     {id:9, ad:"ceyhun9"},
 //     {id:10, ad:"ceyhun10"}
 // ]
-
 
 // oz usulum ile find methodu
 /*
@@ -257,14 +223,9 @@ console.log(man)
 //     return man.id===8
 // })
 
-
-
 // funksiyanin daha qisa yazilisi ile find metodu
 // const manName = men.find(man => man.id === 9)
 // console.log(manName)
-
-
-
 
 // 13. fiter metodu
 
@@ -281,63 +242,70 @@ console.log(man)
 //     {id:8, ad:"ceyhun10"}
 // ]
 
-
 // const filterMethod = men.filter(function(man){
 //     return man.id === 8
 // })
 // console.log(filterMethod)
 
-
-
-
-
 // 14. moterizelerin duzgunluyunu yoxlayan kod yazin
 
+// function duzgunMoterize(moterize){
+//     const arrMoterize = moterize.split("")
+//     const netice = arrMoterize.reduce(function(pre, curr){
+//         console.log(pre, curr)
+//         if( pre < 0){
+//             return pre
+//         }
+//         if(curr === "("){
+//             return ++pre
+//         }
+//         if(curr === ")"){
+//             return --pre
+//         }
+//         return pre
+//     },0)
+//     if(!netice){
+//         console.log("moterizeler duzgundur")
+//     }else{
+//         console.log("moterizeler duzgun deyil!")
+//     }
+// }
+// duzgunMoterize("((()))")
 
-function duzgunMoterize(moterize){
-    const arrMoterize = moterize.split("")
-    const netice = arrMoterize.reduce(function(pre, curr){
-        console.log(pre, curr)
-        if( pre < 0){
-            return pre
-        }
-        if(curr === "("){
-            return ++pre
-        }
-        if(curr === ")"){
-            return --pre
-        }
-        return pre
-    },0)
-    if(!netice){
-        console.log("moterizeler duzgundur")
-    }else{
-        console.log("moterizeler duzgun deyil!")
-    }
+// 15. higher order function,callback function
+
+let myList = [1, 3, 5, 9];
+console.log(myList);
+
+function topla(myList) {
+  newList = [];
+  for (let i = 0; i < myList.length; i++) {
+    newList[i] = myList[i] + 5;
+  }
+  return newList;
 }
-duzgunMoterize("((())")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
+function vur(myList) {
+  newList = [];
+  for (let i = 0; i < myList.length; i++) {
+    newList[i] = myList[i] * 5;
+  }
+  return newList;
+}
+function cix(myList) {
+  newList = [];
+  for (let i = 0; i < myList.length; i++) {
+    newList[i] = myList[i] - 5;
+  }
+  return newList;
+}
+function bol(myList) {
+  newList = [];
+  for (let i = 0; i < myList.length; i++) {
+    newList[i] = myList[i] / 5;
+  }
+  return newList;
+}
+console.log(topla(myList));
+console.log(vur(myList));
+console.log(cix(myList));
+console.log(bol(myList));
